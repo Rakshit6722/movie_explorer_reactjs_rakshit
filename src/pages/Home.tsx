45098af275db4931b932f901822e4cc0569ba626
Rakshit6722 <rakshitchauhan435@gmail.com>
2025-04-29 12:50:11 +0530

    [Rakshit] Add: home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Header from '../components/common/Header/Header'
+import Index from '../components/home/CategorySection/Index'
+import Footer from '../components/common/Footer'
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Index/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+export default Home
