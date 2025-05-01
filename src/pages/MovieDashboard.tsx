aa5db73056f35cde931a13c1d27894d521f32cd6
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-01 11:30:42 +0530

    [Rakshit] Refactor: added a linear progress bar to show while home page is loading

--- src/pages/MovieDashboard.tsx
+++ src/pages/MovieDashboard.tsx
@@ -2,11 +2,43 @@ import React, { Component } from 'react'
 import Header from '../components/common/Header/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/common/Footer'
+import { LinearProgress } from '@mui/material'
+import WithRouter from '../components/hoc/WithRouter'
 
-export class MovieDashboard extends Component {
+export class MovieDashboard extends Component<any> {
+
     render() {
+
+        const { movieLoading } = this.props
+
         return (
             <>
+                {/* <LinearProgress 
+                    variant="indeterminate"
+                    sx={{ 
+                        zIndex: '150',
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        height: '5px',
+                        '& .MuiLinearProgress-bar': {
+                            backgroundColor: '#f02c49',
+                        },
+                        '&.MuiLinearProgress-root': {
+                            backgroundColor: 'rgba(240, 44, 73, 0.2)',
+                        },
+                        '&::before': {
+                            content: '""',
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            right: 0,
+                            height: '100%',
+                            backgroundColor: 'rgba(240, 44, 73, 0.2)',
+                        }
+                    }} 
+                /> */}
                 <div className="flex min-h-screen bg-black text-white">
                     <Header />
                     <div className="flex-1 md:pl-6 pr-0 ml-20 overflow-y-auto">
@@ -18,4 +50,4 @@ export class MovieDashboard extends Component {
     }
 }
 
-export default MovieDashboard
+export default WithRouter(MovieDashboard)
