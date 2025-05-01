9877a9a72c1367537e372b988f188f4d927cd15d
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-01 10:26:51 +0530

    [Rakshit] Refactor: added persist gate for redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,22 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import store from './redux/store.ts'
+import store, { persistor } from './redux/store.ts'
 import { Provider } from 'react-redux'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
       <BrowserRouter>
         <App />
         <ToastContainer />
       </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
