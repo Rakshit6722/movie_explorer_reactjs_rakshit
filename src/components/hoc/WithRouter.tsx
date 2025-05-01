c69c6ed3e4b02fcdfd660623edd3d828ee791b1b
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-01 11:31:51 +0530

    [Rakshit] Refactor: added a movieLoading state

--- src/components/hoc/WithRouter.tsx
+++ src/components/hoc/WithRouter.tsx
@@ -1,6 +1,7 @@
 import { ComponentType } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { RootState } from '../../redux/store';
 
 const WithRouter = (Component: ComponentType<any>) => {
     const NewComponent = (props: any) => {
@@ -8,7 +9,8 @@ const WithRouter = (Component: ComponentType<any>) => {
         const dispatch = useDispatch();
         const location = useLocation();
         const params = useParams()
-        return <Component {...props} params={params} location={location} dispatch={dispatch} navigate={navigate} />;
+        const loading = useSelector((state: RootState) => state.movie.loading);
+        return <Component {...props} params={params} location={location} dispatch={dispatch} navigate={navigate} movieLoading={loading} />;
     };
     return NewComponent;
 };
