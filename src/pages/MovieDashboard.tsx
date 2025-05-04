ad3885bb481bc5b979a6872eeca9e210462370fb
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-05 00:04:57 +0530

    [Rakshit] Refactor: added linear progress bar to show while loading

--- src/pages/MovieDashboard.tsx
+++ src/pages/MovieDashboard.tsx
@@ -8,37 +8,39 @@ import WithRouter from '../components/hoc/WithRouter'
 export class MovieDashboard extends Component<any> {
 
     render() {
-
-        const { movieLoading } = this.props
-
+        const { movieLoading } = this.props;
+        
         return (
             <>
-                {/* <LinearProgress 
-                    variant="indeterminate"
-                    sx={{ 
-                        zIndex: '150',
-                        position: 'fixed',
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        height: '5px',
-                        '& .MuiLinearProgress-bar': {
-                            backgroundColor: '#f02c49',
-                        },
-                        '&.MuiLinearProgress-root': {
-                            backgroundColor: 'rgba(240, 44, 73, 0.2)',
-                        },
-                        '&::before': {
-                            content: '""',
-                            position: 'absolute',
+                {movieLoading && (
+                    <LinearProgress 
+                        variant="indeterminate"
+                        sx={{ 
+                            zIndex: '150',
+                            position: 'fixed',
                             top: 0,
                             left: 0,
                             right: 0,
-                            height: '100%',
-                            backgroundColor: 'rgba(240, 44, 73, 0.2)',
-                        }
-                    }} 
-                /> */}
+                            height: '5px',
+                            '& .MuiLinearProgress-bar': {
+                                backgroundColor: '#f02c49',
+                            },
+                            '&.MuiLinearProgress-root': {
+                                backgroundColor: 'rgba(240, 44, 73, 0.2)',
+                            },
+                            '&::before': {
+                                content: '""',
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                height: '100%',
+                                backgroundColor: 'rgba(240, 44, 73, 0.2)',
+                            }
+                        }} 
+                    />
+                )}
+            
                 <div className="flex min-h-screen bg-black text-white">
                     <Header />
                     <div className="flex-1 md:pl-6 pr-0 ml-20 overflow-y-auto">
