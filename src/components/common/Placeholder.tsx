e016707664c09bb7639e41ab9958ab2e7e24bd4a
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-04 23:57:27 +0530

    [Rakshit] Add: Placeholder component for search page

---
+++ src/components/common/Placeholder.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { FiSearch } from 'react-icons/fi';
+
+export class Placeholder extends Component {
+  render() {
+    return (
+      <div className="flex flex-col items-center justify-center h-full opacity-40">
+        <div className="text-center">
+          <div className="inline-flex items-center justify-center p-5 bg-gray-800/30 rounded-full mb-6">
+            <FiSearch size={40} className="text-gray-500/80" />
+          </div>
+          <h2 className="text-xl font-light text-gray-400/90 mb-2">
+            Search for movies
+          </h2>
+          <p className="text-gray-500/70 text-xs max-w-xs mx-auto">
+            Enter a title or keyword in the search bar above
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Placeholder;
