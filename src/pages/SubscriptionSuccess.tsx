eae0d2addbacab995c4eba7975cfa0e980a9a722
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-09 11:35:08 +0530

    test commit

--- src/pages/SubscriptionSuccess.tsx
+++ src/pages/SubscriptionSuccess.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const SubscriptionSuccess = () => {
+
+  const params = new URLSearchParams(window.location.search)
+  const paymentIntentId = params.get('session_id')
+
+  useEffect(() => {
+    if (paymentIntentId) {
+      updatePaymentStatus()
+    } else {
+      console.error("No payment intent ID found in the URL.")
+    }
+  }, [])
+
+  const updatePaymentStatus = async () => {
+    const response = await axios.get(`https://movie-explorer-rorakshaykat2003-movie.onrender.com/api/v1/subscriptions/success?session_id=${paymentIntentId}`)
+    console.log("Payment status updated:", response.data)
+  }
+
   return (
     <div>
       Payment completed successfully! Thank you for your subscription.
