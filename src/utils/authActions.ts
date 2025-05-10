9b89b6e060b864cd6fe90e4913c39adec7a46e76
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-11 00:09:15 +0530

    [Rakshit] Add: login and logout utility functions for auth

---
+++ src/utils/authActions.ts
@@ -0,0 +1,36 @@
+import { toast } from "react-toastify"
+import { resetUser, setCurrentPlan, setToken, setUser } from "../redux/slices/userSlice"
+import { logoutUser, userNotificationApi } from "../services/api"
+import { requestForToken } from "./fcm"
+
+export const loginUser = async (response: any, dispatch: any, navigate: any) => {
+    dispatch(setUser(response?.data?.user))
+    dispatch(setToken(response?.data?.token))
+    dispatch(setCurrentPlan(response?.data?.user?.active_plan))
+    localStorage.setItem("token", response?.data?.token)
+    navigate('/')
+    const fcmToken = await requestForToken()
+    if (fcmToken) {
+        console.log("fcmToken", fcmToken)
+        await userNotificationApi({
+            device_token: fcmToken,
+            notifications_enabled: true
+        })
+    }
+}
+
+export const logoutUtil = async (dispatch?: any, navigate?: any) => {
+    try {
+        const response = await logoutUser()
+        if (response?.data?.message === "Logout successful") {
+            dispatch(resetUser())
+            localStorage.removeItem("persist:root");
+            localStorage.removeItem("token")
+            localStorage.removeItem("plan")
+            navigate('/')
+            toast.success("Logout successful")
+        }
+    } catch (err: any) {
+        console.log(err)
+    }
+}
