968270a0eba247ee5111cc680b1068ba01e0ec0d
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-13 11:05:19 +0530

    [Rakshit] Refactor: changed toaster styling

--- src/main.tsx
+++ src/main.tsx
@@ -27,7 +27,9 @@ createRoot(document.getElementById('root')!).render(
                 <AuthProvider>
                   <App />
                 </AuthProvider>
-                <ToastContainer />
+                <ToastContainer
+                  toastClassName={'my-toast'}
+                />
               </BrowserRouter>
             </FirebaseProvider>
           </QueryClientProvider>
