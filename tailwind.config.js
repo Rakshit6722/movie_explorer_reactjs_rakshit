7989cd30b04ab05f8a85aa48abdb6de52f84af3e
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-13 12:24:37 +0530

    updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ export default {
     extend: {
       fontFamily: {
         anton: ['Anton', 'sans-serif'],
-      }
+      },
+      animation: {
+        shine: 'shine 3s ease-in-out infinite',
+      },
+      keyframes: {
+        shine: {
+          '0%, 100%': { transform: 'translateX(-100%) skewX(-45deg)' },
+          '50%': { transform: 'translateX(100%) skewX(-45deg)' },
+        }
+      },
     },
   },
   plugins: [],
