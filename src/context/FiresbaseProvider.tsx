0c36533d3a5f99622fb42f7c771ed954cb1a929c
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-14 10:53:43 +0530

    [Rakshit] Refactor: added a notification sound on foreground notification

--- src/context/FiresbaseProvider.tsx
+++ src/context/FiresbaseProvider.tsx
@@ -43,7 +43,7 @@ function FirebaseProvider({ children }: any) {
                         try {
             
                             const audio = new Audio(audioUrl);
-                            audio.volume = 5
+                            audio.volume = 1
                             audio.play()
 
 
