1c91520d85f528e890ef619cf6c77779ca6e5a71
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-15 12:03:15 +0530

    [Rakshit] Refactor: updated conditional conditions

--- src/utils/AuthorizeUser.ts
+++ src/utils/AuthorizeUser.ts
@@ -1,7 +1,5 @@
-import { current } from "@reduxjs/toolkit";
 
 export const authorizeUserForAccessMovie = (currentPlan: any, movie: any, role: string): boolean => {
-
 
     if(role === 'supervisor' || role === 'admin') {
         return true;
@@ -9,21 +7,24 @@ export const authorizeUserForAccessMovie = (currentPlan: any, movie: any, role: 
 
     const moviePlan = movie?.plan
 
+    let isValid = false
+
     if (!currentPlan || !moviePlan) {
-        return false;
+        isValid = false;
     }
     if (currentPlan === 'platinum') {
-        return true;
+        isValid = true;
     }
 
     if (currentPlan === 'gold') {
-        return moviePlan === 'basic' || moviePlan === 'gold';
+        if(moviePlan === 'basic' || moviePlan === 'gold') isValid = true
     }
 
     if (currentPlan === 'basic' || currentPlan === null) {
-        return moviePlan === 'basic';
+        if(moviePlan === 'basic') isValid = true;
     }
     
-    return false;
+    console.log("is valid", isValid)
+    return isValid;
 
 }
