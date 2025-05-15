bff75a1cfe0071a6a9ae99534e9d4dfefe10ed62
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-15 19:32:24 +0530

    [Rakshit] Refactor: fixed imports

--- src/components/home/MainCarousel.tsx
+++ src/components/home/MainCarousel.tsx
@@ -1,4 +1,4 @@
-import React,  from "react";
+import React from "react";
 import Slider from "react-slick";
 import { MdArrowRight } from "react-icons/md";
 import MainCarouselMovieCard from "./MainCarouselMovieCard";
