35240ba48d16e9ca7ee427a66c1890a69b5f8270
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-20 19:53:45 +0530

    firebase config updated

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,20 +3,20 @@ import { getMessaging } from "firebase/messaging";
 import { toast } from "react-toastify";
 
 export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
-  // apiKey: "AIzaSyBD9LE28yQAUD8VgBc3-pGH_vSJvkvq7q0",
-  // authDomain: "fir-messaginservice.firebaseapp.com",
-  // projectId: "fir-messaginservice",
-  // storageBucket: "fir-messaginservice.firebasestorage.app",
-  // messagingSenderId: "753278980485",
-  // appId: "1:753278980485:web:3f4436e86e55a3b05df4ec",
-  // measurementId: "G-YR2NW0T0XW"
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  // projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  // appId: import.meta.env.VITE_FIREBASE_APPID,
+  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+  apiKey: "AIzaSyBD9LE28yQAUD8VgBc3-pGH_vSJvkvq7q0",
+  authDomain: "fir-messaginservice.firebaseapp.com",
+  projectId: "fir-messaginservice",
+  storageBucket: "fir-messaginservice.firebasestorage.app",
+  messagingSenderId: "753278980485",
+  appId: "1:753278980485:web:3f4436e86e55a3b05df4ec",
+  measurementId: "G-YR2NW0T0XW"
 };
 
 export const app = initializeApp(firebaseConfig);
