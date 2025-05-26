f5520cca878646bb13cd3b7031cfe2a32e49262f
Rakshit Chauhan <rakshit.chauhan@magicsw.com>
2025-05-27 01:35:24 +0530

    fixed main carousel animation

--- src/components/home/Index.tsx
+++ src/components/home/Index.tsx
@@ -154,6 +154,15 @@ const Index = () => {
     </Box>
   );
 
+  const mainCarouselAnimProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  transition: { 
+    duration: 0.8,
+    ease: [0.25, 0.1, 0.25, 1],
+  },
+};
+
   const scrollAnimProps = {
     initial: { opacity: 0, y: 35 },
     whileInView: { opacity: 1, y: 0 },
@@ -203,7 +212,7 @@ const Index = () => {
             {renderCarouselSkeleton()}
           </>
         ) : (
-          <motion.div {...scrollAnimProps}>
+          <motion.div {...mainCarouselAnimProps}>
             <MoodFeaturePromo />
             <MainCarousel />
           </motion.div>
